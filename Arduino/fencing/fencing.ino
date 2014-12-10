6a67fd2c458319aa63d6235cdc3618f3ccb0f00e
Alexander Eldeib <aeldeib@college.harvard.edu>
2014-12-09 22:12:47 -0500

    Ace's branch

--- Arduino/fencing/fencing.ino
+++ Arduino/fencing/fencing.ino
@@ -1,5 +1,5 @@
 void setup() {
-    // initialize serial communication at 57600 bits per second:
+    // initialize serial communication at 9600 bits per second:
     Serial.begin(9600);
     pinMode(0, OUTPUT);    // set D0 as output
     pinMode(A0, INPUT);
@@ -10,11 +10,6 @@ void setup() {
 void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  //float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  // Serial.println(sensorValue);
-  //Serial.print(Bean.getBatteryLevel());
 
   if (sensorValue == 0.00)
   {
@@ -36,5 +31,4 @@ void loop() {
   else{
     Bean.setLed(0,0,0);
   }
-  // Bean.sleep(100);
 }
