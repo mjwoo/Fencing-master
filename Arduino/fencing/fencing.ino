ce011c206922e3a072260aac6db907660fc5d323
Jeffrey Zhao <jeffreyzhao@college.harvard.edu>
2014-12-09 22:56:01 -0500

    moved LED code

--- Arduino/fencing/fencing.ino
+++ Arduino/fencing/fencing.ino
@@ -3,32 +3,28 @@ void setup() {
     Serial.begin(9600);
     pinMode(0, OUTPUT);    // set D0 as output
     pinMode(A0, INPUT);
-    digitalWrite(0, HIGH);
+    digitalWrite(0, LOW);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
+  Bean.enableAdvertising(false);
 
   if (sensorValue == 0.00)
   {
     //Serial.write("Touch Scored!\n");
     // Enable the advertising packets
-    Bean.enableAdvertising( true, 1000 );
+    Bean.enableAdvertising( true, 250 );
+    Bean.setLed(0,255,0);
     Bean.sleep(1000);
   }
   else
   {
     Bean.enableAdvertising(false); 
+    Bean.setLed(0,0,0);
   }
   
   bool advertising = Bean.getAdvertisingState();
-  if( advertising ){
-    Bean.setLed(0,255,0);
-    Bean.enableAdvertising(false);
-  }
-  else{
-    Bean.setLed(0,0,0);
-  }
 }
